c97629bb86c64217a890a8dc1045beffc9662694
Vladas Lapinskas <vladas.lapinskas@b-i.com>
2019-11-01 10:12:58 +0000

    Adding .env configuration

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+Dotenv\Dotenv::create(__DIR__. '/../')->load();
+
 define ('DB', [
     'host' => getenv('MYSQL_HOST'),
     'user' => getenv('MYSQL_USER'),
--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+require __DIR__ . '/../config/config.php';
+
 (new Vladas\App(
     new Vladas\Docs\Document()
 ))->get()->run();
